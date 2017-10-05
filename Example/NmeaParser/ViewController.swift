d333d59608e358188cc2e9a34bec88987a8cac61
Johannes Steudle <johannes.steudle@bosch-softtec.com>
2017-10-05 23:38:10 +0200

    Swift compatibility update
    * Update to latest Swift syntax
    * Make it buildable again
    * Fix UnitTests

--- Example/NmeaParser/ViewController.swift
+++ Example/NmeaParser/ViewController.swift
@@ -3,7 +3,7 @@
 //  NmeaParser
 //
 //  Created by tweetjay on 05/31/2017.
-//  Copyright (c) 2017 johannes82. All rights reserved.
+//  Copyright (c) 2017 Johannes Steudle. All rights reserved.
 //
 
 import UIKit
@@ -14,13 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print(NmeaParser.parseSentence(data: "$GPRMC,031849.49,A,5209.028,N,00955.836,E,,,310517,,E*7D"))
+        print(NmeaParser.parseSentence(data: "$GPRMC,031849.49,A,5209.028,N,00955.836,E,,,310517,,E*7D")!)
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 }
 
--- Example/Tests/NmeaParserTest.swift
+++ Example/Tests/NmeaParserTest.swift
@@ -1,45 +1,59 @@
 import Quick
 import Nimble
 
+@testable import NmeaParser
+
 class TableOfContentsSpec: QuickSpec {
     override func spec() {
 
-        describe("test various string inputs") {
-            
-            let validTestStrings = [
-                "$GPGGA,092750.000,5321.6802,N,00630.3372,W,1,8,1.03,61.7,M,55.2,M,,*76",
+        describe("test string input") {
+            let gsvTestStrings = [
+               "$GPGSV,3,1,11,10,63,137,17,07,61,098,15,05,59,290,20,08,54,157,30*70",
+               "$GPGSV,3,2,11,02,39,223,19,13,28,070,17,26,23,252,,04,14,186,14*79",
+               "$GPGSV,3,3,11,29,09,301,24,16,09,020,,36,,,*76",
+               "$GPGSV,3,1,11,10,63,137,17,07,61,098,15,05,59,290,20,08,54,157,30*70",
+               "$GPGSV,3,2,11,02,39,223,16,13,28,070,17,26,23,252,,04,14,186,15*77",
+               "$GPGSV,3,3,11,29,09,301,24,16,09,020,,36,,,*76"
+            ]
+         
+            let gsaTestStrings = [
                 "$GPGSA,A,3,10,07,05,02,29,04,08,13,,,,,1.72,1.03,1.38*0A",
-                "$GPGSV,3,1,11,10,63,137,17,07,61,098,15,05,59,290,20,08,54,157,30*70",
-                "$GPGSV,3,2,11,02,39,223,19,13,28,070,17,26,23,252,,04,14,186,14*79",
-                "$GPGSV,3,3,11,29,09,301,24,16,09,020,,36,,,*76",
-                
+                "$GPGSA,A,3,10,07,05,02,29,04,08,13,,,,,1.72,1.03,1.38*0A"
+            ]
+         
+            let rmcTestStrings = [
                 "$GPRMC,092750.000,A,5321.6802,N,00630.3372,W,0.02,31.66,280511,,,A*43",
-                
+                "$GPRMC,092751.000,A,5321.6802,N,00630.3371,W,0.06,31.66,280511,,,A*45"
+            ]
+         
+            let ggaTestStrings = [
                 "$GPGGA,092751.000,5321.6802,N,00630.3371,W,1,8,1.03,61.7,M,55.3,M,,*75",
-                "$GPGSA,A,3,10,07,05,02,29,04,08,13,,,,,1.72,1.03,1.38*0A",
-                "$GPGSV,3,1,11,10,63,137,17,07,61,098,15,05,59,290,20,08,54,157,30*70",
-                "$GPGSV,3,2,11,02,39,223,16,13,28,070,17,26,23,252,,04,14,186,15*77",
-                "$GPGSV,3,3,11,29,09,301,24,16,09,020,,36,,,*76",
-                
-                "$GPRMC,092751.000,A,5321.6802,N,00630.3371,W,0.06,31.66,280511,,,A*45"]
-            
+                "$GPGGA,092750.000,5321.6802,N,00630.3372,W,1,8,1.03,61.7,M,55.2,M,,*76"
+            ]
+         
+         context("GGA") {
             it("GGA [0] should return nil") {
-                let testLocation = NmeaParser.parseSentence(data: validTestStrings[0])
+                let testLocation = NmeaParser.parseSentence(data: ggaTestStrings[0])
                 expect(testLocation).to(beNil())
             }
-            
+         }
+         
+         context("GSA") {
             it("GSA [1] should return nil") {
-                let testLocation = NmeaParser.parseSentence(data: validTestStrings[1])
+                let testLocation = NmeaParser.parseSentence(data: gsaTestStrings[0])
                 expect(testLocation).to(beNil())
             }
-            
+         }
+         context("GSV") {
             it("GSV [2] should return nil") {
-                let testLocation = NmeaParser.parseSentence(data: validTestStrings[2])
+                let testLocation = NmeaParser.parseSentence(data: gsvTestStrings[0])
                 expect(testLocation).to(beNil())
             }
-            
+         }
+         
+         context("RMC") {
             it("RMC [5] should return a valid location object") {
-                let testLocation = NmeaParser.parseSentence(data: validTestStrings[5])
+                let testLocation = NmeaParser.parseSentence(data: rmcTestStrings[0])
                 
                 expect(testLocation?.timestamp.timeIntervalSince1970) == 1306574870
                 expect(testLocation?.coordinate.latitude) == 53.216802
@@ -50,7 +64,7 @@ class TableOfContentsSpec: QuickSpec {
             
             it("RMC [11] should return a valid location object") {
                 
-                let testLocation = NmeaParser.parseSentence(data: validTestStrings[11])
+                let testLocation = NmeaParser.parseSentence(data: rmcTestStrings[1])
                 
                 expect(testLocation?.timestamp.timeIntervalSince1970) == 1306574871
                 expect(testLocation?.coordinate.latitude) == 53.216802
@@ -58,6 +72,7 @@ class TableOfContentsSpec: QuickSpec {
                 expect(testLocation?.speed) == 0.06
                 expect(testLocation?.course) == 31.66
             }
+         }
         }
     }
 }
